8ba835d446403738bcbf739669647dac2b91a6ad
aditi-m-iyer <aditi.iyer@ltts.com>
2021-11-29 14:21:15 +0530

    final commit

--- BloodBankMVC/Controllers/HomeController.cs
+++ BloodBankMVC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace BloodBankMVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About our Blood Bank";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page.";
 
             return View();
         }
